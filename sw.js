514ecc91f25c5c52fab0bd72f8c9de0017176305
Pyrzii <pyry.mannisto@gmail.com>
2026-02-27 16:56:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,46 @@
+const CACHE_NAME = 'maastodata-cache-v1';
+const PRECACHE_ASSETS = [
+    'index.html',
+    'manifest.json',
+    'styles.css',
+    'script.js',
+];
 
+// Install event: Cache all the specified assets
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).
+            then((cache) => cache.addAll(PRECACHE_ASSETS))
+    );
+});
+
+// Fetch event: Network-first strategy for dynamic content
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request)
+            .then((response) => {
+                // Cache the response
+                const responseClone = response.clone();
+                caches.open(CACHE_NAME).then((cache) => {
+                    cache.put(event.request, responseClone);
+                });
+                return response;
+            })
+            .catch(() => caches.match(event.request))
+    );
+});
+
+// Clear old caches
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.filter((cacheName) => {
+                    return cacheName !== CACHE_NAME;
+                }).map((cacheName) => {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
